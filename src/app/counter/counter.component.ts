f2c0c69d86df84d42af71b253f6826ffbca26265
Anuroop Joy <anuroop.joy@hrblock.com>
2021-08-07 13:54:25 +0530

    interface

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { AppState } from '../app.interfaces';
 import { decrement, increment, incrementByAmount } from './counter.actions';
 import { countSelector } from './counter.selectors';
 
@@ -10,7 +11,7 @@ import { countSelector } from './counter.selectors';
   styleUrls: ['./counter.component.scss'],
 })
 export class CounterComponent {
-  constructor(private store: Store<{ counter: { count: number } }>) {
+  constructor(private store: Store<AppState>) {
     // this.count$ = this.store.select('counter', 'count');
     this.count$ = this.store.select(countSelector);
   }
